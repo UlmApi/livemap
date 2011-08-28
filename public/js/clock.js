0951afac6c800862c9959822f0dcb486000ece4b
Michael Müller <mich@elmueller.net>
2011-08-28 15:18:46 +0200

    added service free period notice

--- public/js/clock.js
+++ public/js/clock.js
@@ -2,22 +2,39 @@
 	place this content into livemap.js, 
 	delete <script src=clock.s> from layout.html 
 */
+
+var nullen = function(i) {
+	return (i < 10) ? i = '0' + i : i;
+};
+
+var getOffset = function(d) {
+	/* summertime for germany, 2011 */
+	if ((d.getUTCMonth() >= 3 && d.getUTCDay() >= 27) && 
+		(d.getUTCMonth() <= 10 && d.getUTCDay() <= 30))
+	    return 2;
+	else 
+	    return 1;
+}
+
 $(document).ready(function(){
-	window.setInterval(function() {
+	window.setInterval(function() {		
 		var d = new Date();
-		/* summertime for germany, 2011 */
-		if ((d.getUTCMonth() >= 3 && d.getUTCDay() >= 27) && 
-			(d.getUTCMonth() <= 10 && d.getUTCDay() <= 30))
-		    offset = 2;
-		else 
-		    offset = 1;
+		var offset = getOffset(d);
 
-		var hrs = (d.getUTCHours() + 2) % 24;
-		var mins = d.getUTCMinutes();
-
-		var secs = d.getUTCSeconds();
-		secs = (secs < 10) ? secs = '0' + secs : secs;
+		var hrs = nullen((d.getUTCHours() + offset) % 24);
+		var mins = nullen(d.getUTCMinutes());
+		var secs = nullen(d.getUTCSeconds());
 
 		$("#clock").html(hrs + ':' + mins + ':' + secs);
 	}, 1000);
+
+
+	/* is it a service free period= */
+	var d = new Date();
+	var offset = getOffset(d);
+	if (
+		(((d.getUTCHours() + offset) % 24) > 23 && d.getUTCMinutes() > 30) || 
+		(((d.getUTCHours() + offset) % 24)) < 6)
+		$("#warning").show();
+	
 });
