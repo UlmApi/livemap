7ece9249a15275a7131e1dc240e45ba11eb3644f
Benjamin Erb <mail@benjamin-erb.de>
2011-08-28 16:34:05 +0200

    replaced event simulator with gtfs-based data generator

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ var ejs = require('ejs');
 var io = require('socket.io');
 
 
-var events = require(path.join(__dirname,"lib","event-simulator","event-simulator.js"));
+//var events = require(path.join(__dirname,"lib","event-simulator","event-simulator.js"));
+var gtfsEvents = require(path.join(__dirname,"lib","gtfs-parser","gtfs-timetable-parser.js"));
 var mapDataGenerator = require(path.join(__dirname,"lib","map-data-generator","map-data-generator.js"));
 var PathNormalizer = require(path.join(__dirname,"lib","path-normalizer","path-normalizer.js"));
+var Gtfs = require(path.join(__dirname, "lib", "gtfs-parser", "gtfs-loader"));
 
 var app = express.createServer();
 
@@ -22,27 +24,41 @@ app.configure(function() {
 	app.set('view engine', 'html');
 });
 
-mapDataGenerator.gen(process.env.GTFS_PATH || path.join(__dirname,"gtfs","ulm"), function(mapData) {
+var gtfs = Gtfs(process.env.GTFS_PATH || path.join(__dirname,"gtfs","ulm"), function(gtfsData){
 
-	//TODO: @cmichi: sanitize/parse stops and trips
+	mapDataGenerator.gen(process.env.GTFS_PATH || path.join(__dirname,"gtfs","ulm"), function(mapData) {
+
+		//calculate normalized shapes
+		var pathNormalizer = PathNormalizer(mapData.getShapes());
+
+		require(path.join(__dirname, '/routes/site'))(app, mapData.getStops(), mapData.getShapes(),mapData.getTrips());
+
+		io = io.listen(app);
+		io.sockets.on('connection', function (socket) {
+		});
+
+		/* event simulator, throws an event every 10 secs. */
+		gtfsEvents.init(gtfsData, 10000, function(step) { /* 7 = speed (0..) */
+			console.dir(step);
+			var p = Math.floor(step.progress * 10);
+
+			step['pointList'] = [];
+			step['foo'] = p;
+			for(var i = 0;i<10;i++){
+				step['pointList'].push(pathNormalizer.getNormalizedPath("87001")[p+i]);
+			
+			}
+		
+			io.sockets.emit('event', step);
+		});
+
+
+		app.listen(parseInt(process.env.PORT) || 7777); 
+		console.log('Listening on ' + app.address().port);
+
+	});
+
 	
-	//calculate normalized shapes
-	var pathNormalizer = PathNormalizer(mapData.getShapes());
-
-	require(path.join(__dirname, '/routes/site'))(app, mapData.getStops(), mapData.getShapes(),mapData.getTrips());
-
-	io = io.listen(app);
-	io.sockets.on('connection', function (socket) {
-	});
-
-	/* event simulator, throws an event every 10 secs. */
-	events.init(7, function(step) { /* 7 = speed (0..) */
-		io.sockets.emit('event', step);
-	});
-
-
-	app.listen(parseInt(process.env.PORT) || 7777); 
-	console.log('Listening on ' + app.address().port);
-
 });
+
 
